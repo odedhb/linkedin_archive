4fe7d4b983f64699c12eee53105b16840d5d8591
Oded Breiner <odedhb@gmail.com>
2023-07-24 13:43:05 +0300

    Update linkedin_archive_messages.js

--- linkedin_archive_messages.js
+++ linkedin_archive_messages.js
@@ -1,12 +1,10 @@
 timer = setInterval(() => {
     // select all messages
-    items = document.querySelectorAll('.msg-selectable-entity__entity');
+    const items = document.querySelectorAll('.msg-selectable-entity__entity');
     for (let i = 0; i < items.length; i++) {
         items[i].click();
     }
-    // click delete button
-    buttons = document.querySelectorAll('.msg-conversation-card__toggle-archive-btn');
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].click();
-    }
-})
+    const archiveButton = document.querySelector('[title="Archive"]');
+    console.log({archiveButton});
+    archiveButton.click();
+},1000)
